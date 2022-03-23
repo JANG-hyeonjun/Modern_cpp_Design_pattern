5a30f6a3f78271b03d6257d6c86809dc42538b63
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-03-23 22:56:48 +0900

    make simple Builder pattern example make the html

---
+++ Builder/Builder_01/Builder_01/Builder_pattern_study.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <memory>
+
+//웹페이지를 그리기 위해 2개의 컴포넌트를 생성해야 한다고 하면 다음과 같이 만들수 있다.
+
+
+class HtmlBuilder;
+
+class HtmlElement
+{
+public:
+	std::string name;
+	std::string text;
+	std::vector<HtmlElement> elements;
+	const size_t indent_size = 2;
+
+	HtmlElement() { }
+	HtmlElement(const std::string& name, const std::string& text) : name(name), text(text){ }
+
+	std::string  str(int indent = 0) const
+	{
+		std::ostringstream oss;
+		std::string i(indent_size * indent, ' ');
+		oss << i << "<" << name << ">" << std::endl;
+		if (text.size() > 0)
+			oss << std::string(indent_size * (indent + 1), ' ') << text <<std::endl;
+
+		for (const auto& e : elements)
+		{
+			oss << (std::string)"1!!!!!" << std::endl;
+			oss << e.str(indent + 1);
+		}
+	
+		oss << i << "</" << name << ">" << std::endl;
+		return oss.str();
+	}
+	
+	/*static std::unique_ptr<HtmlBuilder> build(std::string root_name)
+	{
+		return std::make_unique<HtmlBuilder>(root_name);
+	}*/
+
+};
+
+//단순 빌더 패턴 2022.03.23
+class HtmlBuilder
+{
+public:
+	HtmlElement root;
+
+	HtmlBuilder(std::string root_name)
+	{
+		root.name = root_name;
+	}
+
+	void add_child(std::string child_name, std::string child_text)
+	{
+		HtmlElement e(child_name, child_text);
+		root.elements.emplace_back(e);
+	}
+
+	std::string str()
+	{
+		return root.str();
+	}
+};
+
+
+int main()
+{
+
+	std::string words[] = { "hello", "world" };
+	std::ostringstream oss;
+
+	/*oss << "<ul>";
+	for (auto w : words)
+	{
+		oss << " <li>" << w << "</li>";
+	}
+	oss << "</ul>";
+
+	printf(oss.str().c_str());
+	
+	std::cout << std::endl;
+	*/
+	//이러한 구조는 만약 앞에점을 찍거나 순서대로 번호를 매겨야 한다면 어떻게 수정? -> 상당히 힘들다 
+	//그래서 class를 한번 정의해보면 다음과 같다. 
+
+	/*HtmlElement list{ "ul","" };
+
+	for (auto w : words)
+	{
+		list.elements.emplace_back(HtmlElement{"li",w});
+	}
+
+	printf(list.str().c_str());*/
+
+	//단순 빌더 클래스 사용 
+	HtmlBuilder builder("ul");
+	builder.add_child("li", "hello");
+	builder.add_child("li", "world");
+	std::cout << builder.str() << std::endl;
+
+	
+
+
+	return 0;
+}
